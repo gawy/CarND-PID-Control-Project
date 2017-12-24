5dba25ec2623a4ae334401bfd90129b0a836b0ed
Andrew Garkavyi <andrew.gawy@gmail.com>
2017-12-24 00:23:05 -0800

    Super basic PID controller

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -6,16 +7,30 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
+double prev_cte = 0;
+
 PID::PID() {}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Kd = Kd;
+  this->Ki = Ki;
 }
 
 void PID::UpdateError(double cte) {
+  p_error = cte * Kp;
+
+  d_error = (prev_cte - cte) * Kd;
+  prev_cte = cte;
+
+  i_error += cte * Ki;
+
+  cout<<"Errors: p="<<p_error << ", d="<<d_error <<", i="<<i_error<<endl;
 }
 
 double PID::TotalError() {
+  return p_error - d_error - i_error;
 }
 
